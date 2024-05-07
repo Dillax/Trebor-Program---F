ded70a9dbd9eceeaab65163381b8f74232c6cc9d
Dillax <163461719+Dillax@users.noreply.github.com>
2024-05-07 01:19:54 -0300

    3

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const tempoObjetivo1 = new Date("2024-05-29T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
 const tempoObjetivo4 = new Date("2024-05-20T00:00:00");
-const tempoObjetivo4 = new Date("2024-08-01T00:00:00");
+const tempoObjetivo5 = new Date("2024-08-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
 
