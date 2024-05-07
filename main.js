de9bb7c3750e1fd80a3f65156ad90fc83baf2eb5
Dillax <163461719+Dillax@users.noreply.github.com>
2024-05-07 01:26:41 -0300

    4

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return [dias,horas,minutos,segundos];
     } else {
-        return [0,0,0,0];
+        return [0,0,0,0,0];
     }
 }
 
