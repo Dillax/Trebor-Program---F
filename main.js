a7a5cbb3560c1068f424fe815d1b2ea375c7b957
Dillax <163461719+Dillax@users.noreply.github.com>
2024-06-26 20:23:48 -0300

    t

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-05-29T00:00:00");
+const tempoObjetivo1 = new Date("2024-08-29T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2025-01-01T00:00:00");
-const tempoObjetivo4 = new Date("2024-05-20T00:00:00");
+const tempoObjetivo4 = new Date("2024-09-20T00:00:00");
 const tempoObjetivo5 = new Date("2024-08-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4,tempoObjetivo5];
